f67aaca6b2cf026dd0152c747c9b82bd2ecc4cb4
faiz <faizbilhaqq95@gmail.com>
2023-09-19 10:43:02 +0700

    updated success page

--- frontend/src/pages/uploadJSON.js
+++ frontend/src/pages/uploadJSON.js
@@ -1,19 +1,58 @@
 import "./uploadJSON.css"
-import { Text } from '@chakra-ui/react'
+import { AbsoluteCenter, Text } from '@chakra-ui/react'
 import { CheckCircleIcon } from '@chakra-ui/icons'
+import { Center, Square, Circle } from '@chakra-ui/react'
+import { Box, Flex } from "@chakra-ui/react"
+import { Stack, HStack, VStack } from '@chakra-ui/react'
+import { Button, ButtonGroup } from '@chakra-ui/react'
+
 
 const Upload = () => {
     return ( 
         <div className="container">
-            <div className="square">
-            <Text fontSize='4xl'>Success!!</Text>
-
-            <CheckCircleIcon boxSize={20} ml={20} mt={9}  />
-
-            <Text  mt={30} ml="auto" mr="auto" fontSize='xl'>JSON file uploaded and processed succesfully</Text>
 
 
-            </div>
+            <VStack >
+          
+         
+          
+          <CheckCircleIcon mt={20}  boxSize={40}  color={"green"} />
+          <Text color={"green"} as='b' fontSize='5xl'>Succesfully</Text>
+          <Text fontSize='5xl'>JSON file uploaded and processed</Text>
+          <Text fontSize='xl'>*Result will be sent via Email</Text>
+
+          
+
+          <Button mt={20}  bg={"green"} color={"white"} h={"50px"}  w="25%">Home</Button>
+
+
+        
+          
+
+
+
+          
+            </VStack>
+
+            {/* <div className="contentUpload"> */}
+            
+            
+            
+
+
+            {/* </div> */}
+            
+
+           {/* <CheckCircleIcon boxSize={40}  color={"green"} /> */}
+           {/* JSON file uploaded and processed succesfully */}
+
+            
+
+
+
+
+
+
         </div>
      );
 }
