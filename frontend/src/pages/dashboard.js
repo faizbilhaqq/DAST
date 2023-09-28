3918125e062dea28a73d738e9bcb2eba169d06ae
faiz <faizbilhaqq95@gmail.com>
2023-09-28 20:59:02 +0700

    add url and json input

--- frontend/src/pages/dashboard.js
+++ frontend/src/pages/dashboard.js
@@ -2,15 +2,15 @@ import "./dashboard.css";
 import { Text } from '@chakra-ui/react';
 import { Input } from '@chakra-ui/react';
 import { Textarea } from '@chakra-ui/react';
-import { Tabs, TabPanels, TabPanel } from '@chakra-ui/react';
 import { Button } from '@chakra-ui/react';
 import { useState } from 'react';
 import telkomHQ2 from "../assets/telkomHQ2.jpg"
 import { useNavigate } from 'react-router-dom';
-
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
 
 const Dash = () => {
     const [email, setEmail] = useState('');
+    const [Url, setUrl] = useState("");
     const [jsonData, setJsonData] = useState(null);
     const [isActive, setisActive] = useState(true);
     const navigate = useNavigate();
@@ -23,16 +23,27 @@ const Dash = () => {
             return;
         }
 
-        if (!jsonData) {
-            alert('Please select a JSON file.');
+        if (!jsonData && !Url) {
+            alert('Please select a JSON file or enter URL');
             return;
         }
+
+
 
         // Prepare data to send to the server
         const formData = new FormData();
         formData.append('email', email);
-        formData.append('jsonFile', new Blob([JSON.stringify(jsonData)], { type: 'application/json' }));
+        if (jsonData){
+            
+            formData.append('jsonFile', new Blob([JSON.stringify(jsonData)], { type: 'application/json' }));
+        }
 
+        if (Url){
+            
+            formData.append('URL', JSON.stringify(Url));
+        }
+        
+        
         try {
             // Send a POST request to localhost:3001/upload-json
             const response = await fetch('http://localhost:3001/upload-json', {
@@ -42,14 +53,15 @@ const Dash = () => {
 
             if (response.ok) {
                 navigate("/loading")
-                setTimeout(navigate('/uploadJSON'), 30000)
+                await setTimeout(navigate('/uploadJSON'), 30000)
                 
 
                 
-                alert('JSON file and email sent successfully.');
+                // alert('JSON file and email sent successfully.');
 
                 // Clear the form after successful submission
                 setEmail('');
+                setUrl('');
                 setJsonData(null);
 
 
@@ -94,10 +106,34 @@ const Dash = () => {
                 <div className="form2">
                 <Text fontSize='4xl' as='b' >Scan Here!</Text>
                 <br />
-                <Text mb={5} fontSize='l'  >Put your Email and JSON file here to Scan</Text>
+                <Text mb={5} fontSize='l'  >Put your Email and JSON/URL here to Scan</Text>
 
                     <form action="">
 
+                    {/* <div className="textLabel2">
+                    <Text as='b' fontSize='sm'  >Email</Text>
+                    <Input  type="email" placeholder='Enter Your Email Address' value={email} onChange={(e) => setEmail(e.target.value)}  />
+                    </div>
+
+                    <div >
+                    <Text as='b' fontSize='sm'  >
+                        JSON
+                    </Text>
+                    <br />
+                    <input type="file" className={isActive ? "hidden": "notHidden"} accept=".json" onChange={handleFileChange}  placeholder='JSON' height={130} /> 
+                    
+                    
+
+
+                    </div> */}
+
+                    <Tabs isFitted variant='enclosed'>
+                    <TabList mb='1em'>
+                        <Tab _selected={{ color: 'white', bg: 'blue.300' }} >JSON</Tab>
+                        <Tab _selected={{ color: 'white', bg: 'blue.300' }} >URL</Tab>
+                    </TabList>
+                    <TabPanels>
+                        <TabPanel pt={0} >
                     <div className="textLabel2">
                     <Text as='b' fontSize='sm'  >Email</Text>
                     <Input  type="email" placeholder='Enter Your Email Address' value={email} onChange={(e) => setEmail(e.target.value)}  />
@@ -114,6 +150,25 @@ const Dash = () => {
 
 
                     </div>
+                        </TabPanel>
+
+                        <TabPanel pt={0} >
+
+                        <div className="textLabel2">
+                        <Text as='b' fontSize='sm'  >Email</Text>
+                        <Input  type="email" placeholder='Enter Your Email Address' value={email} onChange={(e) => setEmail(e.target.value)}  />
+                        </div>
+
+                        <div className="textLabel2">
+                        <Text as='b' fontSize='sm'  >URL</Text>
+                        <Input  type="text" placeholder='Enter URL' value={Url} onChange={(e) => setUrl(e.target.value)}  />
+                        </div>                        
+
+                        </TabPanel>
+
+
+                    </TabPanels>
+                    </Tabs>
 
                     <Button  onClick={handleSubmit} className="button2" bg='black'  _hover={{ bg: '#999999' }} color="white" size='md'>
                     Submit
